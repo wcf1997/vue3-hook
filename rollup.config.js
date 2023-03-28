a5d905b9fcee32e00201e917bbd2f5f0fded1679
吴春发 <972985271@qq.com>
2023-03-28 10:05:39 +0800

    jsx测试

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,65 @@
-import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import vueJsx from "rollup-plugin-vue-jsx-compat"
+import resolve from "@rollup/plugin-node-resolve";
+import jsx from "acorn-jsx";
 import esbuild from "rollup-plugin-esbuild";
 
+function setOutput(){
+  const path = 'dist/'; // 编译后代码存放地址
+  const output = [];
 
+  if (process.env.NODE_ENV === 'production') {
+    ['iife', 'es', 'umd'].forEach((item) => {
+      output.push({
+        dir: path + item,
+        format: item,
+        name: 'v3-usehook',
+      });
+    });
+  } else {
+    output.push({
+      dir: path,
+      format: 'es',
+      name: 'v3-usehook',
+    });
+  }
+
+  return output
+
+}
 export default {
   input: "./src/index.ts", // 打包入口
-  output: {
-    // 打包出口
-    file: "dist/index.js",
-    format: "umd", // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
-    name: "utilibs", // cdn方式引入时挂载在window上面用的就是这个名字
-    sourcemap: true,
-  },
+  // output: {
+  //   // 打包出口
+  //   file: "dist/index.js",
+  //   format: "umd", // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
+  //   name: "utilibs", // cdn方式引入时挂载在window上面用的就是这个名字
+  //   sourcemap: true,
+  // },
+  output: setOutput(),
+  acornInjectPlugins: [jsx()],
+
+  // 忽略打包文件数组
+  external: ["vue", "@antv/g2"],
   plugins: [
     // 打包插件
     vueJsx(),
     esbuild({
       jsxFactory: "vueJsxCompat",
     }),
-    commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
-    typescript(), // 解析TypeScript
+     commonjs({
+       extensions,
+       sourceMap: true
+     }),
+    typescript({
+      lib: ["es5", "es6", "dom"],
+      target: "es5", // 输出目标
+      noEmitOnError: true, // 运行时是否验证ts错误
+    }),
+    resolve({
+      mainFields: ["module", "main", "browser"]
+    }),
     babel({
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
       babelHelpers: "bundled"
